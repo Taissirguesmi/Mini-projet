a5999b8d6bef5210114ac02931f0ce2cc35be5b3
Taissirguesmi <Taissir.guesmi@hotmail.com>
2021-01-08 23:42:25 +0100

    mini projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MusicienneComponent } from './musicienne/musicienne.component';
+import { AddMusicienneComponent } from './add-musicienne/add-musicienne.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateMusicienneComponent } from './update-musicienne/update-musicienne.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicienneComponent,
+    AddMusicienneComponent,
+    UpdateMusicienneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
